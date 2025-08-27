18c26d0ec5e3bd25bf375e5cb5a5bd889e9760b8
Your Name <your.email@example.com>
2025-08-26 20:50:42 -0400

    Initial commit

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,48 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable } from "hardhat/config";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: process.env.SEPOLIA_RPC_URL as string,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY as string],
+    },
+  },
+  typechain: {
+    outDir: "typechain",
+  }, 
+};
+
+export default config;
